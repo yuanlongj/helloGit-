614175924ca2180c9cfd58e96848a503ee612cde
yuanlongj <18829013504@163.com>
2021-03-07 10:49:39 +0800

    添加1

--- hello_git/src/main/java/com/atguigu/git/hello_git.java
+++ hello_git/src/main/java/com/atguigu/git/hello_git.java
@@ -5,5 +5,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class hello_git {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git1");
     }
 }
